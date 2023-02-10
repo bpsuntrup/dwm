535992321aaa41c2b191c2d6e1e316cc026473dc
Benjamin Paul Suntrup <benjamin.p.suntrup@gmail.com>
2023-02-10 13:48:04 -0600

    config.h

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 6;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,10 +13,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_green1[]      = "#33FF99";
+static const char col_green2[]      = "#009966";
+static const char col_green_gray[]  = "#669966";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg           border   */
+	[SchemeNorm] = { col_gray3, col_gray1,   col_green_gray },
+	[SchemeSel]  = { col_gray4, col_green2,  col_green1  },
 };
 
 /* tagging */
