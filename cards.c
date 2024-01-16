e77d46238240477d3b359aa50940f5f161aae267
Benjamin Suntrup <benjamin.p.suntrup@gmail.com>
2024-01-16 12:14:09 -0600

    cards.c: a new layout resembling a deck of cards
    This depends on th Gaps layout to work correctly. Add "#include "cards.c""
    to your "config.h" to use.

---
+++ cards.c
@@ -0,0 +1,26 @@
+void
+cards(Monitor *mon) {
+	unsigned int i, n, nw, nh;
+	Client *c;
+
+	for(n = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next), n++);
+	if(n == 0)
+		return;
+	
+	nw = mon->ww;
+	nh = mon->wh;
+
+    unsigned int gap = mon->gap->gappx * 2; /* quirky default, makes this change dependent on gaps */
+	
+	for(i = 1, c = nexttiled(mon->clients); c; c = nexttiled(c->next)) {
+		resize(c, 
+            ((n-i) * (gap + c->bw)) + mon->gap->gappx,  
+            ((n-i) * (gap + c->bw)) + mon->wy + mon->gap->gappx,
+            nw - (n * (gap + c->bw)),
+            nh - (n * (gap + c->bw)),
+            0
+        );
+        i++;
+
+	}
+}
